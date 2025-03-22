4c96eb995850806e319ae9903fb4be80fc291f78
Andrey <AAMutlu20@codingburgas.bg>
2025-03-22 20:22:00 +0200

    Complete portfolio

---
+++ Portfolio/Portfolio/Controllers/Main.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers;
+
+public class Main : Controller
+{
+    public IActionResult Portfolio()
+    {
+        return View();
+    }
+}
